08016a6fbf26cb0f98909466d4109ba17f74f239
Alex <diagraphic@yahoo.com>
2021-05-27 13:53:57 -0400

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import vercel from '@sveltejs/adapter-vercel'
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: vercel(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
